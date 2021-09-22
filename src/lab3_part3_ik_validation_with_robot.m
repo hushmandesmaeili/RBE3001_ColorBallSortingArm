991d68ae38ea0989de9a32ef274c6dda05c5e892
Robinson <jorobinson@ak120d-rb-p-u24.wpi.edu>
2021-09-22 13:08:41 -0400

    added plot3d function, not complete

--- src/lab3_part3_ik_validation_with_robot.m
+++ src/lab3_part3_ik_validation_with_robot.m
@@ -34,11 +34,11 @@ graph = graphing("part3_ik_validation.csv", pp);
 
 try
     %define these position vectors in mm
-    p1 = [45 54 124];
+    p1 = [45; 54; 124];
     q1 = pp.ik3001(p1);
-    p2 = [100 0 195];
+    p2 = [100; 0; 195];
     q2 = pp.ik3001(p2);
-    p3 = [41 -113 111];
+    p3 = [41; -113; 111];
     q3 = pp.ik3001(p3);
 
     %Create empty matrix 10x4x4
@@ -47,25 +47,33 @@ try
     pause(2);
     
     close all;
+    
+    M = zeros(1, 4); %t, x, y, z
+    c = 0;
+    
+    q = pp.measured_js(1,0);
+    q = q(1,:);
+    mod.plot_arm(q);
+    drawnow;
+    
     tic
-    T = zeros(5, 4, 4);
-    c = 1;
+        
     %While loop runs for 60 seconds
     %Improvement note: Make non-blocking code, without while loop
-    while (toc < 6)
-%         g = floor(toc)
-%         disp(c)
+    while (toc < 4)
+        g = floor(toc)
+        disp(c)
         if floor(toc) > c
             c = c + 1;
         end
         
-        if c == 1
+        if c == 0
             pp.servo_jp(q1);
-        elseif c == 2
+        elseif c == 1
             pp.servo_jp(q2);
-        elseif c == 3
+        elseif c == 2
             pp.servo_jp(q3);
-        elseif c == 4
+        elseif c == 3
             pp.servo_jp(q1);
         end
         
@@ -76,7 +84,7 @@ try
         graph.record();
     end
     
-    graph.plotMotion(q1, q2, q3);
+    graph.plot3DMotion(q1, q2, q3);
     
     
 catch exception
