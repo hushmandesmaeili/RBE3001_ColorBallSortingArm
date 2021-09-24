c590e0753ae677eb1b77be0399fce444b37a780b
Robinson <jorobinson@ak120d-rb-p-u24.wpi.edu>
2021-09-23 20:07:18 -0400

    lab complete, all sign offs complete

--- src/lab3_part3_ik_validation_with_robot.m
+++ src/lab3_part3_ik_validation_with_robot.m
@@ -28,27 +28,27 @@ frame = Frame3D();
 
 mod = Model(pp, frame);
 
-graph = graphing("part3_ik_validation.csv", pp);
+% graph = graphing("part3_ik_validation.csv", pp);
 
 
 
 try
     %define these position vectors in mm
-    p1 = [45; 54; 124];
-    q1 = pp.ik3001(p1);
+    p1 = [45; 54; 24];
     p2 = [100; 0; 195];
-    q2 = pp.ik3001(p2);
     p3 = [41; -113; 111];
+    q1 = pp.ik3001(p1);
+    q2 = pp.ik3001(p2);
     q3 = pp.ik3001(p3);
 
     %Create empty matrix 10x4x4
     pp.closeGripper();
-    pp.interpolate_jp([0 0 0], 1000);
+    pp.interpolate_jp(q1, 900);
     pause(2);
     
     close all;
     
-    M = zeros(1, 4); %t, x, y, z
+    M = zeros(1, 4); %t, x, y, z, q1, q2, q3
     c = 0;
     
     q = pp.measured_js(1,0);
@@ -68,23 +68,61 @@ try
         end
         
         if c == 0
-            pp.servo_jp(q1);
+%             pp.servo_jp(q2);
+            pp.interpolate_jp(q2, 900);
         elseif c == 1
-            pp.servo_jp(q2);
+%             pp.servo_jp(q3);
+            pp.interpolate_jp(q3, 900);
         elseif c == 2
-            pp.servo_jp(q3);
-        elseif c == 3
-            pp.servo_jp(q1);
+%             pp.servo_jp(q1);
+            pp.interpolate_jp(q1, 900);
         end
         
         q = pp.measured_js(1,0);
         q = q(1,:);
         mod.plot_arm(q);
         drawnow;
-        graph.record();
+        M(end+1, 1) = toc;
+        M(end, 2:4) = pp.position(q); %position
+        M(end, 5:7) = q; %joint angle
+%         graph.record();
     end
     
-    graph.plot3DMotion(q1, q2, q3);
+    %Plot of Tip position vs Time
+%         subplot(2, 1, 1)
+%         hold on
+%         plot(M(2:end, 1), M(2:end, 2), 'r-') %End Effector X Position
+%         plot(M(2:end, 1), M(2:end, 3), 'g-') %End Effector Y Position
+%         plot(M(2:end, 1), M(2:end, 4), 'b-') %End Effector Z Position
+%         legend('End Effector X Position', 'End Effector Y Position', 'End Effector Z Position')
+%         title("Plot of End Effector Position vs Time")
+%         xlabel('Time (s)')
+%         ylabel('End Effector Position (mm)')
+%         hold off
+%         
+%         subplot(2, 1, 2)
+%         hold on
+%         plot(M(2:end, 1), M(2:end, 5), 'r-') %End Effector X Position
+%         plot(M(2:end, 1), M(2:end, 6), 'g-') %End Effector Y Position
+%         plot(M(2:end, 1), M(2:end, 7), 'b-') %End Effector Z Position
+%         legend('q1 angle', 'q2 angle', 'q3 angle')
+%         title("Plot of Joint Angle vs Time")
+%         xlabel('Time (s)')
+%         ylabel('Joint Angle (deg)')
+%         hold off
+        
+          %Trajectory of this part
+          plot3(M(2:end,2), M(2:end, 3), M(2:end, 4));
+          title('Plot of Interpolated End Effector Path');
+          xlabel('x (mm)');
+          ylabel('y (mm)');
+          zlabel('z (mm)');
+          
+%           writematrix(M(2:end, 2:4), 'RawMotion_TaskSpaceCoord_Trajectory.csv');
+
+
+
+%     graph.plot3DMotion(q1, q2, q3);
     
     
 catch exception
