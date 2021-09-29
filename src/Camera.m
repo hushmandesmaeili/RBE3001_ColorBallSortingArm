c340af42eef956f0ad40c6c1ea1f475cbf6d7148
Robinson <jorobinson@ak120d-rb-p-u23.wpi.edu>
2021-09-29 15:03:29 -0400

    signoff lab1 complete

--- src/Camera.m
+++ src/Camera.m
@@ -1,4 +1,4 @@
-classdef Camera
+classdef Camera < handle
     % CAMERA Example Camera class for RBE 3001 Lab 5
     %   You can add your image processing in this camera class,
     %   as well as any other functions related to the camera.
@@ -9,35 +9,26 @@ classdef Camera
         POSE_PLOT = false;  
         DEBUG_BALLDETECTION = false;
         
-        % Image Processing Variables
-        
-        % Colors
-        
         % Properties
         params;
         cam;
         cam_pose;
+        cam_imajl;
     end
     
     methods
         function self = Camera()
             % CAMERA Construct an instance of this class
-            self.cam = webcam(); % Get camera object
+            self.cam = webcam(2); % Get camera object
             self.params = self.calibrate(); % Run Calibration Function
-            
+            [self.cam_imajl, self.cam_pose] = self.getCameraPose();
         end
 
         function shutdown(self)
             % SHUTDOWN shutdown script which clears camera variable
             clear self.cam;
         end
-        
-        function [pose, color] = detectBestBall(self)
-            % DETECTBESTBALL Detect Balls + Report back positions + colors
-            % Note: you do NOT need to use this function, this is just a recommendation
-            
-        end
-        
+      
         function params = calibrate(self)
             % CALOBRATE Calibration function
             % This function will run the camera calibration, save the camera parameters,
@@ -53,16 +44,26 @@ classdef Camera
             try
                 disp("Clear surface of any items, then press any key to continue");
                 pause;
-                camcalib; % Change this if you are using a different calibration script
+%                 camcalib; % Change this if you are using a different calibration script
+                camParam;
                 params = cameraParams;
+                disp("This is REEE Frogg:")
                 disp("Camera calibration complete!");
             catch exception
-                getReport(exception);
+                msg = getReport(exception);
+                disp(msg)
                 disp("No camerea calibration file found. Plese run camera calibration");
             end          
         end
         
-        function pose = getCameraPose(self)
+        % Returns an undistorted camera image
+        function img = getImage(self)
+            raw_img =  snapshot(self.cam);
+            [img, new_origin] = undistortFisheyeImage(raw_img, self.params.Intrinsics, 'OutputView', 'full');
+        end
+
+        
+        function [newIs, pose] = getCameraPose(self)
             % GETCAMERAPOSE Get transformation from camera to checkerboard frame
             % This function will get the camera position based on checkerboard.
             % You should run this function every time the camera position is changed.
@@ -75,22 +76,34 @@ classdef Camera
 
             % 1. Capture image from camera
             raw_img =  snapshot(self.cam);
-            % 2. Undistort Image based on params
-            [img, new_origin] = undistortImage(raw_img, self.params, 'OutputView', 'full');
+%             % 2. Undistort Image based on params
+            [img, newIs] = undistortFisheyeImage(raw_img, self.params.Intrinsics, 'OutputView', 'full');
             % 3. Detect checkerboard in the image
             [imagePoints, boardSize] = detectCheckerboardPoints(img);
-            % 4. Adjust imagePoints so they are in the same frame of
-            % reference as original image
-            imagePoints = imagePoints + new_origin;
-            % 5. Compute transformation
-            [R, t] = extrinsics(imagePoints, self.params.WorldPoints, self.params);
+            % 4. Compute transformation
+            [R, t] = extrinsics(imagePoints, self.params.WorldPoints, self.params.Intrinsics);
             
             pose = [   R,    t';
                     0, 0, 0, 1];
                 
             if self.POSE_PLOT
-                axesPoints = worldToImage(self.params, R, t, [0 0 0; 0 50 0; 50 0 0]);
                 
+                figure(10)
+                newImgPoints = [];
+                c = 1;
+                for i = 0:25:175
+                    for j = 0:25:125
+                        newImgPoints(c,1) = i;
+                        newImgPoints(c,2) = j;
+                        newImgPoints(c,3) = 0;
+                        c = c + 1;
+                    end
+                end
+                
+                newGridPoints = worldToImage(newIs, R, t, newImgPoints);
+
+                axesPoints = worldToImage(newIs, R, t, [0 0 0; 0 50 0; 50 0 0]);
+
                 x1 = [axesPoints(1, 1), axesPoints(2, 1)]';
                 y1 = [axesPoints(1, 2), axesPoints(2, 2)]';
                 
@@ -99,15 +112,16 @@ classdef Camera
                 y2 = [axesPoints(1, 2), axesPoints(3, 2)]';
                 
                 img = insertText(img, axesPoints(3, 1:2), 'X Axis', 'TextColor', 'red', 'FontSize', 18);
-                
+                hold on 
                 imshow(img)
                 title('Undistorted Image with checkerboard axes');
+                viscircles(newGridPoints, ones(length(newGridPoints),1)*5);
+                hold off
                 
                 line(x1, y1, 'linewidth', 5, 'Color', 'green');
                 line(x2, y2, 'linewidth', 5, 'Color', 'red');
                 
             end     
-        end        
+        end
     end
 end
-
